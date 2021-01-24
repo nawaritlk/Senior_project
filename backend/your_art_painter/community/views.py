f7449bce0044fba40c66940d5108a9816e827877
SUKASAMA <money0700@gmail.com>
2021-01-24 22:27:16 +0700

    update menu bar into backend

--- backend/your_art_painter/community/views.py
+++ backend/your_art_painter/community/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def community(request):
-    return HttpResponse("This is communnity page")
+    return render(request, 'community.html')
