250b029478fcc4666dce038d48de009ff5ac0d1c
SUKASAMA <money0700@gmail.com>
2021-04-26 15:34:19 +0700

    try register

--- new_backend/your_art_painter/register/admin.py
+++ new_backend/your_art_painter/register/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import register
+from .models import user_info
 
 # Register your models here.
-admin.site.register(register)
+admin.site.register(user_info)
