4db11ec6d0399879c3ad9cf589de522ea3b6bb3d
SUKASAMA <money0700@gmail.com>
2021-04-27 19:38:05 +0700

    register without auto login and alert-danger doesn't work

--- new_backend/your_art_painter/register/models.py
+++ new_backend/your_art_painter/register/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
 from django.db.models.fields import CharField
+# from django.forms import widgets
 
 # Create your models here.
 class user_info(models.Model):
     #user = models.OneToOneField(User, on_delete=CASCADE)
     email = models.EmailField(blank=False)
-    password = models.CharField(max_length=100,blank=False)
+    password = models.CharField(max_length=100, blank=False)
     confirm_password = models.CharField(max_length=100,blank=False)
 
 
--- new_backend/your_art_painter/register/views.py
+++ new_backend/your_art_painter/register/views.py
@@ -3,77 +3,30 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserForm
+# from .forms import UserForm
 from django.views.decorators.csrf import csrf_exempt
+from .models import user_info
 
 # Create your views here.
 
+
 @csrf_exempt
 def user_info(request):
-    form = UserForm()
-    # if request.method == "GET":
-    #     print('success1')
-    #     form = UserCreationForm(request.POST or None)
-    #     if form.is_valid():
-    #         print('success2')
-    #         form.save()
-    #         print(form)
-    #     context = {'form': form}
-    #     return render(request, 'register.html', context)
-    # else:
-    #     form = UserCreationForm(request.POST)
-        
     if request.method == 'POST':
-        print('It look OK--1')
-        form = UserForm(request.POST or None)
-        print('It look OK--2')
-        print(form)
-        print(form.errors)
-        if form.is_valid():
-            print('It look OK--3')
-            form.save()
-        #     print('It look OK--3')
-        #     if User.objects.filter(email=form.cleaned_data['email']).exists():
-        #         print('It look OK--4 exists email')
-        #         return render(request, 'register.html', {
-        #             'form': form,
-        #             'error_message': 'Email already exists.'
-        #         })
-        #     elif form.cleaned_data['password'] != form.cleaned_data['confirm_password']:
-        #         print('It look OK--5  password not match')
-        #         return render(request, 'register.html', {
-        #             'form': form,
-        #             'error_,message': 'Password do not match.'
-        #         })
-        #     else:
-        #         user = User.objects.create_user(
-        #             form.cleaned_data['email'],
-        #             form.cleaned_data['password']
-        #         )
-        #         user.save()
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password', 000000)
+        confirm_password = request.POST.get('confirm_password', 000000)
+        if password == confirm_password:
+            if User.objects.filter(email=email).exists():
+                messages.error(request, 'Email Already Exist')
+                # print('Email Already Exist')
+            else:
+                registerdata = User.objects.create_user(username=username, email=email, password=password)
+                registerdata.save()
+                return redirect('homepage')
+        else:
+            messages.error(request, 'Password and Confirm Password Not Matched')
+            # print("password not match")
 
-        #         return HttpResponseRedirect('homepage.html')
-        # else:
-        #     user_form = UserForm()
-        context = {'form': form}
-
-        return render(request, 'register.html', context)
-
-    # if request.method == 'POST':
-    #     Email = request.POST.get('email')
-    #     Password = request.POST.get('password', 000000)
-    #     Confirm_password = request.POST.get('confirm_password', 000000)
-    #     print(Password, Confirm_password)
-    #     if Password == Confirm_password:
-    #         if User.objects.filter(Email=Email).exists():
-    #             #messages.error(request, 'Email Already Exist')
-    #             print('Email Already Exist')
-    #         else:
-    #             registerdata = register(Email=Email, Password=Password, Confirm_password=Confirm_password)
-    #             registerdata.save()
-    #             return redirect('homepage')
-    #     else:
-    #         #messages.error(request, 'Password and Confirm Password Not Matched')
-    #         print("password not match")
-
-    # return render(request, 'register.html', context = dict)
+    return render(request, 'register.html')
