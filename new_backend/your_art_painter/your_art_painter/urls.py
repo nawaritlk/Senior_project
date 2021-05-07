90d0df63599897e4163ee342af469b2bef4c4df8
NawaritFrong <nawaritfrong@gmail.com>
2021-05-07 12:55:19 +0700

    fit homepage

--- new_backend/your_art_painter/your_art_painter/urls.py
+++ new_backend/your_art_painter/your_art_painter/urls.py
@@ -3,6 +3,7 @@ from django.urls import path,include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,7 +16,10 @@ urlpatterns = [
     path('auth/', include('authentication.urls')),
     path('temp/', include('temp.urls')),
     path('upload/', include('create_your_art.urls'))
-]
+    
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
